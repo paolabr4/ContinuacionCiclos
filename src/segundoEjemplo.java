1a8ef550e6bd5a3d71695f25aec898079587b05b
paolabr4 <90522319+paolabr4@users.noreply.github.com>
2022-07-02 08:33:14 -0500

    Numeros del 10 al 1

---
+++ src/segundoEjemplo.java
@@ -0,0 +1,11 @@
+public class segundoEjemplo {
+    public static void main(String[] args){
+        // Imprimir los numero del 10 al 1
+
+        int numero = 10;
+        while (numero>=1){
+            System.out.println("numero: " +numero);
+            numero = numero-1;
+        }
+    }
+}
